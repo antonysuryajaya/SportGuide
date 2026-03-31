a1e4e2e145cd2435ad3eca195b923dd501db91fd
antonysuryajaya <antony.suryajaya@gmail.com>
2026-03-31 14:13:39 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Bell } from 'lucide-react-native';
+import { colors, fontType } from './assets/theme';
+import ListBlog from './src/components/ListBlog';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [loaded] = useFonts(fontType);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" backgroundColor={colors.green()} />
+      <View style={styles.header}>
+        <Text style={styles.title}>SportGuide.</Text>
+        <Bell color={colors.black()} size={24} />
+      </View>
+      <View style={styles.listCategory}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <View style={{ ...category.item, marginLeft: 24 }}>
+            <Text style={{ ...category.title, color: colors.blue() }}>
+              Popular
+            </Text>
+          </View>
+          <View style={category.item}>
+            <Text style={category.title}>makanan</Text>
+          </View>
+          <View style={category.item}>
+            <Text style={category.title}>guide</Text>
+          </View>
+          <View style={category.item}>
+            <Text style={category.title}>trend</Text>
+          </View>
+          <View style={category.item}>
+            <Text style={category.title}>langkah awal</Text>
+          </View>
+          <View style={{ ...category.item, marginRight: 24 }}>
+            <Text style={category.title}>Lifestyle</Text>
+          </View>
+        </ScrollView>
+      </View>
+      <ListBlog styles={styles} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.green(),
+    shadowColor: colors.green(),
+  },
+  header: {
+    paddingHorizontal: 24,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    height: 52,
+    paddingTop: 8,
+    paddingBottom: 4,
+    backgroundColor: colors.green(),
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: 'Pjs-ExtraBold',
+    color: colors.black(),
+  },
+  listCategory: {
+    paddingVertical: 10,
+  },
+  listBlog: {
+    paddingVertical: 10,
+    gap: 10,
   },
 });
+const category = StyleSheet.create({
+  item: {
+    paddingHorizontal: 14,
+    paddingVertical: 10,
+    borderRadius: 25,
+    alignItems: 'center',
+    backgroundColor: colors.orange(0.08),
+    marginHorizontal: 5
+  },
+  title: {
+    fontFamily: 'Pjs-SemiBold',
+    fontSize: 14,
+    lineHeight: 18,
+    color: colors.orange(),
+  },
+});
+
---
+++ colors.js
@@ -0,0 +1,9 @@
+const colors = {
+    orange: (opacity = 1) => `rgba(255, 87, 34, ${opacity})`,
+    blue: (opacity = 1) => `rgba(33, 150, 243, ${opacity})`,
+    green: (opacity = 1) => `rgba(205, 254, 64, ${opacity})`,
+    black: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+    darkModeBlack: (opacity = 1) => `rgba(27, 27, 27, ${opacity})`,
+    darkModeBlue: (opacity = 1) => `rgba(146, 156, 241, ${opacity})`,
+}
+export default colors
