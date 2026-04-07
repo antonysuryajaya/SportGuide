b7e15d7426be2163ae05b9fcf461262be573d233
worsenlife <antony.suryajaya@gmail.com>
2026-04-07 14:09:28 +0700

    Add existing file

---
+++ src/data/blogs.js
@@ -0,0 +1,47 @@
+export const BlogList = [
+  {
+    id: 1,
+    title: 'exploring the best form in creating healthy body',
+    category: 'Guide',
+    image:
+      'https://images.unsplash.com/photo-1526506118085-60ce8714f8c5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Zml0bmVzc3xlbnwwfHwwfHx8MA%3D%3D',
+    createdAt: 'Nov 10, 2023',
+    totalComments: 89,
+  },
+  {
+    id: 2,
+    title: 'Evolution item for sport',
+    category: 'Popular',
+    image:
+      'https://images.unsplash.com/photo-1583454110551-21f2fa2afe61?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Zml0bmVzc3xlbnwwfHwwfHx8MA%3D%3D',
+    createdAt: 'Nov 10, 2023',
+    totalComments: 89,
+  },
+  {
+    id: 3,
+    title: 'new form for women sport',
+    category: 'Latest',
+    image:
+      'https://plus.unsplash.com/premium_photo-1674421795169-e4550d50ece2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGZpdG5lc3N8ZW58MHx8MHx8fDA%3D',
+    createdAt: 'Nov 10, 2023',
+    totalComments: 89,
+  },
+  {
+    id: 4,
+    title: 'best food for you body',
+    category: 'Food',
+    image:
+      'https://plus.unsplash.com/premium_photo-1664648005723-3cd53a75b44d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zml0bmVzcyUyMGZvb2R8ZW58MHx8MHx8fDA%3D',
+    createdAt: 'Nov 10, 2023',
+    totalComments: 89,
+  },
+  {
+    id: 5,
+    title: 'best clothes material that good for your body',
+    category: 'Music',
+    image:
+      'https://images.unsplash.com/photo-1637710900042-6077c0377d70?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGZpdG5lc3MlMjBjbG90aGVzfGVufDB8fDB8fHww',
+    createdAt: 'Nov 10, 2023',
+    totalComments: 89,
+  },
+];
---
+++ src/data/categories.js
@@ -0,0 +1,22 @@
+export const CategoryList = [
+  {
+    id: 1,
+    categoryName: 'Popular',
+  },
+  {
+    id: 2,
+    categoryName: 'Latest',
+  },
+  {
+    id: 3,
+    categoryName: 'Guide',
+  },
+  {
+    id: 4,
+    categoryName: 'Health',
+  },
+  {
+    id: 5,
+    categoryName: 'Lifestyle',
+  },
+];
