c631044046d91d5e83ae126901040a65bf0beee6
worsenlife <antony.suryajaya@gmail.com>
2026-04-14 20:36:49 +0700

    Submit tugas 4

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -7,6 +7,8 @@ export const BlogList = [
       'https://images.unsplash.com/photo-1526506118085-60ce8714f8c5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Zml0bmVzc3xlbnwwfHwwfHx8MA%3D%3D',
     createdAt: 'Nov 10, 2023',
     totalComments: 89,
+    totalLikes:2300,
+    content: `to create a pefect body need a very good and precise step tp do that we will tell you in here.`,
   },
   {
     id: 2,
@@ -16,6 +18,8 @@ export const BlogList = [
       'https://images.unsplash.com/photo-1583454110551-21f2fa2afe61?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Zml0bmVzc3xlbnwwfHwwfHx8MA%3D%3D',
     createdAt: 'Nov 10, 2023',
     totalComments: 89,
+    totalLikes:6700,
+    content: `every week new item for sport will created so we will tell you whichone is worth to buy.`,
   },
   {
     id: 3,
@@ -25,6 +29,8 @@ export const BlogList = [
       'https://plus.unsplash.com/premium_photo-1674421795169-e4550d50ece2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGZpdG5lc3N8ZW58MHx8MHx8fDA%3D',
     createdAt: 'Nov 10, 2023',
     totalComments: 89,
+    totalLikes:5000,
+    content: `the woman body have different need and form to do we will tell you which form is good for you.`,
   },
   {
     id: 4,
@@ -34,6 +40,8 @@ export const BlogList = [
       'https://plus.unsplash.com/premium_photo-1664648005723-3cd53a75b44d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zml0bmVzcyUyMGZvb2R8ZW58MHx8MHx8fDA%3D',
     createdAt: 'Nov 10, 2023',
     totalComments: 89,
+    totalLikes:7900,
+    content: `best food that can be eaten so you will not ruin your progress.`,
   },
   {
     id: 5,
@@ -43,5 +51,7 @@ export const BlogList = [
       'https://images.unsplash.com/photo-1637710900042-6077c0377d70?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGZpdG5lc3MlMjBjbG90aGVzfGVufDB8fDB8fHww',
     createdAt: 'Nov 10, 2023',
     totalComments: 89,
+    totalLikes:10000,
+    content: `material that is good and not hurt or give allergy and comfortable to use.`,
   },
 ];
---
+++ src/data/profiledata.js
@@ -0,0 +1,9 @@
+export const ProfileData = {
+  profilePict:
+    'https://plus.unsplash.com/premium_photo-1737392495524-8752e43cbc93?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHByb2ZpbGUlMjBjb3dib3l8ZW58MHx8MHx8fDA%3D',
+  name: 'Arthur Morgan',
+  createdAt: '14 april, 1863',
+  blogPosted: 29,
+  following: 3000,
+  follower: 3000,
+};
